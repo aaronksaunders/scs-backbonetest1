f0cc131a70d30c3f73f72fae0a228ce57ad4a34b
Aaron Saunders <aaron@clearlyinnovative.com>
2015-03-05 15:23:30 -0500

    cleaned up restapi adapter to make is more streamlined, removed the caching

--- app/lib/alloy/sync/restapi.js
+++ app/lib/alloy/sync/restapi.js
@@ -1,110 +1,23 @@
 function InitAdapter() {
-	db = Titanium.Database.open("http_client_cache");
-	db.execute("CREATE TABLE IF NOT EXISTS REQUESTS (URL_HASH STRING, RESPONSE TEXT, UPDATED_AT INTEGER)");
-	db.close();
+
 	return {};
-}
-
-function _prune_cache(seconds, _showAlert) {
-	var row,
-	    origCount;
-	null == seconds && ( seconds = options.pruneSeconds);
-	db = Titanium.Database.open("http_client_cache");
-	var row = db.execute("SELECT COUNT(*) FROM REQUESTS WHERE UPDATED_AT < DATETIME('now','-" + seconds + " seconds')");
-	origCount = row && 0 !== row.rowCount ? row.field(0) : 0;
-	Ti.API.debug(" num to purge " + origCount);
-	db.execute("DELETE FROM REQUESTS WHERE UPDATED_AT < DATETIME('now','-" + seconds + " seconds')");
-	row = db.execute("SELECT COUNT(*) FROM REQUESTS WHERE UPDATED_AT < DATETIME('now','-" + seconds + " seconds')");
-	Ti.API.debug("remaining after purge " + (row && 0 !== row.rowCount ? row.field(0) : 0));
-	_showAlert && alert("Purged " + origCount + " records from cache");
-	return db.close();
-}
-
-function _compute_url_hash(_options) {
-	return url_hash = Ti.Utils.md5HexDigest(_options.type + _options.url + _options.data);
-}
-
-function _get_cached_response(seconds, _url_hash) {
-	var cachedAt,
-	    responseText,
-	    row;
-	db = Titanium.Database.open("http_client_cache");
-	null == seconds && ( seconds = options.cacheSeconds);
-	row = db.execute("SELECT RESPONSE, UPDATED_AT FROM REQUESTS WHERE URL_HASH=? AND UPDATED_AT > DATETIME('now','-" + seconds + " seconds')", _url_hash);
-	responseText = row && 0 !== row.rowCount ? row.field(0) : null;
-	cachedAt = row && 0 !== row.rowCount ? row.field(1) : null;
-	row.close();
-	db.close();
-	if (null != responseText) {
-		Ti.API.info(" cache hits " + _url_hash);
-		return {
-			responseText : responseText,
-			cached : true,
-			cached_at : cachedAt,
-			status : 200
-		};
-	}
-}
-
-function _exists_in_cache(_url_hash) {
-	var count,
-	    row,
-	    _ref;
-	row = db.execute("SELECT COUNT(*) FROM REQUESTS WHERE URL_HASH=?", _url_hash);
-	count = row && 0 !== row.rowCount ? row.field(0) : 0;
-	row.close();
-	return null != ( _ref = count > 0) ? _ref : {
-		"true" : false
-	};
-}
-
-function _save_to_cache(_response, _options) {
-	var urlHash;
-	if (_response.status >= 400 || _response.cached)
-		return;
-	db = Titanium.Database.open("http_client_cache");
-	urlHash = _compute_url_hash(_options);
-	if (_exists_in_cache(urlHash)) {
-		Ti.API.info("updated cache " + urlHash);
-		db.execute("UPDATE REQUESTS SET RESPONSE=?, UPDATED_AT=CURRENT_TIMESTAMP WHERE URL_HASH=?", _response.responseText, urlHash);
-	} else {
-		Ti.API.info("cached " + urlHash);
-		db.execute("INSERT INTO REQUESTS (RESPONSE, URL_HASH, UPDATED_AT) VALUES (?,?,CURRENT_TIMESTAMP)", _response.responseText, urlHash);
-	}
-	return db.close();
 }
 
 function apiCall(_options, _callback) {
 
 	_options.preventActivityIndicator = true;
 
-	_prune_cache();
-	var urlHash = _compute_url_hash(_options);
-	if (false && ( response = _get_cached_response(90, urlHash))) {
-		try {
-			_callback({
-				success : true,
-				text : response.responseText || null,
-				data : data = JSON.parse(response.responseText) || null
-			});
-			return;
-		} catch (EE) {
-			Ti.API.error("bad cache entry " + _options.url);
-		}
-		true !== _options.preventActivityIndicator && activityWindow.hide();
-	}
 	var xhr = Ti.Network.createHTTPClient({
 		timeout : ONE_MINUTE / 2
 	});
 	xhr.options = _options;
 	xhr.open(_options.type, _options.url);
-	null !== _options.maxTimeout &&
-	void 0 != _options.maxTimeout && activityWindow.setMaxTime(_options.maxTimeout);
+
 	xhr.onload = function() {
-		true !== _options.preventActivityIndicator && activityWindow.hide();
+
 		var data = null;
 		try {
-			_save_to_cache(xhr, xhr.options);
+
 			data = JSON.parse(xhr.responseText);
 			Ti.API.debug("apiCall response Text::" + xhr.responseText);
 			_callback({
@@ -123,7 +36,7 @@ function apiCall(_options, _callback) {
 		}
 	};
 	xhr.onerror = function() {
-		true !== _options.preventActivityIndicator && activityWindow.hide();
+
 		_callback({
 			success : false,
 			text : xhr.responseText
@@ -133,7 +46,7 @@ function apiCall(_options, _callback) {
 	for (var header in _options.headers)
 	xhr.setRequestHeader(header, _options.headers[header]);
 	_options.beforeSend && _options.beforeSend(xhr);
-	true !== _options.preventActivityIndicator && activityWindow.show(_options.ActivityMessage || "Loading");
+
 	Ti.API.debug("_options.url: " + _options.url);
 	Ti.API.debug("_options.type: " + _options.type);
 	Ti.API.debug("_options.data: " + JSON.stringify(_options.data));
@@ -180,9 +93,9 @@ function Sync(method, model, opts) {
 	}
 	!Ti.Android && (params.headers["Content-Type"] = "application/json; charset=utf-8");
 	var callbackOptions = function(_resp) {
-		if (_resp.success)
+		if (_resp.success) {
 			params.success(_resp.data || _resp.text, _resp.text);
-		else {
+		} else {
 			try {
 				params.error && params.error(JSON.parse(_resp.text), _resp.text);
 			} catch (EE) {
@@ -192,22 +105,38 @@ function Sync(method, model, opts) {
 			model.trigger("error");
 		}
 	};
-	if (opts.JSON)
+
+	// if there is a model id then set the id on the URL
+	// we are assuming the endpoint follows the normal REST API
+	// convention
+	debugger;
+	var modelId = (model[model.idAttribute] || model.get(model.idAttribute));
+	if (modelId) {
+		params.url = params.url + '/' + modelId;
+	}
+
+	if (opts.JSON) {
 		params.data = model.toJSON();
-	else if (opts.data || !model || "create" != method && "update" != method) {
+	} else if (opts.data || !model || "create" != method && "update" != method) {
 		if (opts.data) {
 			var query = "";
-			for (var i in opts.data)
-			query += i + "=" + opts.data[i] + "&";
+			for (var i in opts.data) {
+				query += i + "=" + opts.data[i] + "&";
+			}
 			params.url += "?" + query.substring(0, query.length - 1);
-			Ti.API.debug("THE URL " + params.url);
 			params.data = null;
 		}
-	} else
+	} else {
 		params.data = JSON.stringify(model.toJSON());
+	}
+
+	// display the URL to be used for API call
+	Ti.API.debug("THE URL " + params.url);
+
 	switch (method) {
 	case "delete":
 	case "update":
+
 		apiCall(params, function(_r) {
 			callbackOptions(_r);
 			model.trigger( _r ? "fetch change sync" : "error");
@@ -222,9 +151,7 @@ function Sync(method, model, opts) {
 		break;
 
 	case "read":
-		if (model[model.idAttribute]) {
-			params.url = params.url + '/' + model[model.idAttribute];
-		}
+
 		apiCall(params, function(_r) {
 			callbackOptions(_r);
 			model.trigger( _r ? "fetch sync" : "error");
@@ -232,19 +159,7 @@ function Sync(method, model, opts) {
 	}
 }
 
-//var activityWindow = require("activityIndicator");
-
 var ONE_MINUTE = 6e4;
-
-var options = {
-	retryCount : 0,
-	cacheSeconds : 3e3,
-	pruneSeconds : 252e4
-};
-
-Ti.App.addEventListener("app.purge.cache", function(_options) {
-	_prune_cache(0, _options.showAlert);
-});
 
 var _ = require("alloy/underscore")._;
 
@@ -260,4 +175,4 @@ module.exports.afterModelCreate = function(Model) {
 	Model = Model || {};
 	Model.prototype.config.Model = Model;
 	return Model;
-}; 
+};
